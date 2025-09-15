75536ea8c19567f469c6e90fddd7bcea91189a99
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2025-09-15 12:18:10 +0200

    dict improvement

--- 05-dicts/d.py
+++ 05-dicts/d.py
@@ -1,16 +1,23 @@
-def load_terms():
+def load_terms(fname, input, output):
     d = {}
-    f = open("terms.csv")
-    for line in f.read().split("\n"):
-        if len(line) > 0 and line[0] != '#':
+    f = open(fname) # get file handle
+    for line in f.read().split("\n"): # read file content and split into lines.
+        if len(line) > 0 and line[0] != '#': # filter out empty lines and comments
+            # example line: "lemma , grundform ; lemma , base form ; N"
             fields = line.split(";")
-            english = fields[1].strip()
-            swedish = fields[0].strip()
-            for k in english.split(","):
-                for v in swedish.split(","):
+            # example fields: ['lemma , grundform ', ' lemma , base form ', ' N']
+            keys = fields[input].strip().split(",")
+            # example keys: ['lemma ',' grundform']
+            values = fields[output].strip().split(",")
+            for k in keys:
+                # example k: 'lemma '
+                for v in values:
                     d.setdefault(k.strip(),[]).append(v.strip())
-    f.close()
+    f.close() # release file handle
     return d
 
-d = load_terms()
-print(d["slice"])
+d = load_terms("terms.csv",0,1)
+w = input("word?")
+while w != '':
+    print(d[w])
+    w = input("word?")
