03c840f1cf2dcdd730f74e5fa85b49e5b94fd464
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2025-09-25 09:46:19 +0200

    single rotation

--- 08-matrices-modify.py
+++ 08-matrices-modify.py
@@ -1,12 +1,78 @@
 
+# None : empty cell
+# anything else: occupied
 
-def display(m):
+width = 80
+height = 50
+
+def rotate_group(g,i,j):
+  save = g[i][j]
+  g[i][j] = g[i+1][j] 
+  g[i+1][j] = g[i+1][j+1]
+  g[i+1][j+1] = g[i][j+1]
+  g[i][j+1] = save
+
+def occupied(cell):
+  return cell != None
+
+def is_single_group(g,i,j):
+  return occupied(g[i][j]) + occupied(g[i][j+1]) + occupied(g[i+1][j+1]) + occupied(g[i+1][j]) == 1
+
+def rotate_all_groups(offset,g):
+  for i in range(offset,height-1,2):
+    for j in range(offset,width-1,2):
+      if is_single_group(g,i,j):
+        rotate_group(g,i,j)
+
+def make_glider(g,i,j,o):
+  g[i][j] = o
+  g[i][j+1] = o
+  g[i+2][j] = o
+  g[i+2][j+1] = o
+
+def make_wall(g,i,j,h,o):
+  for k in range(i,i+h):
+    g[k][j] = o
+    g[k][j+1] = o
+
+def display(g):
   print("\033[1;1H")
-  print("vvvvvvvv")
-  for row in m:
-    print(">",end="")
-    for x in row:
-        print(x or " ", end="")
-    print("<")
-  print("^^^^^^^^^")
+  for row in g:
+    for cell in row:
+      if cell == None:
+        print(" ", end="")
+      else:
+        print(cell, end="")
+    print()
+
+grid=[]
+for i in range(height):
+  grid.append([ None ] * width)
+
+  
+make_glider(grid,4,5,'o')
+make_wall(grid,7,15,12,"#")
+
+for i in range(10000):
+  display(grid)
+  # input()
+  rotate_all_groups(i % 2,grid)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
