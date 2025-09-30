fd0e15658a2a2d5bf7bb63a1fa5c2b97b120d1a0
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2025-09-30 09:46:46 +0200

    cheat

---
+++ 10-particles.py
@@ -0,0 +1,11 @@
+
+# timestep = 0.01
+# G = 150
+# M = 1
+# Position in [-100,100]²
+
+# bypass Zelle's graphics interface
+
+# def set_circle_position(c,v):
+#     currentPos = c.getCenter()
+#     c.move(v.x-currentPos.getX(),v.y-currentPos.getY()) 
